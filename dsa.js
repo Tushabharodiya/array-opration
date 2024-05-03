fcb69b04d815a0cd1902afffdbd6ffe781482b52
Tushabharodiya <147471467+Tushabharodiya@users.noreply.github.com>
2024-05-03 15:08:34 +0530

    Add files via upload

---
+++ dsa.js
@@ -0,0 +1,58 @@
+
+//add new element
+// let arr = [1, 2, 3, 4, 8, 9]
+
+// let position = 2;
+// let add = 20;
+
+// for (let i = arr.length-1 ;i >=position;  i--) {
+//     arr[i+1] =arr[i]
+//     if (i==position) {
+//         arr[position]=add
+//     }
+// }
+// console.log(arr);
+
+
+//remove element
+
+// let arr = [1, 2, 3, 4, 5]
+
+// let pos = 2;
+
+// for (let i = pos;i<arr.length-1;i++) {
+//     if (i == pos) {
+//         arr[i] = arr[i-1];
+//     }
+//     console.log(arr[i]);
+// }
+// console.log(arr);
+
+
+//update array element
+
+
+// let arr = [1, 2, 3, 4, 5, 6]
+
+// let newelement = 20;
+// position = 3;
+
+// for (let i = 0; i < arr.length; i++) {
+//         if (i == position - 1) {
+//                 arr[i] = newelement;
+//         }
+// }
+// console.log(arr);
+
+//3.prime number
+// for (let i = 1; i <= 100; i++) {
+//     let found = false;
+//     for (let j = 2; j <= i; j++) {
+//         if (i % j == 0 && j != i) {
+//             found = true;
+//         }
+//     }
+//     if (!found) {
+//         console.log(i);
+//     }
+// }
